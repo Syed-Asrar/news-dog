72a3238a0cf8981dafaaa7a269e4494a1a312e73
Syed Asrar Zahoor <81914950+Syed-Asrar@users.noreply.github.com>
2022-11-07 21:54:45 +0530

    changed prop of news in app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default class App extends Component {
     return (
       <div> 
         <Navbar/>
-        <News articles = {test.articles}/>
+        <News articles={[]}/>
       </div>
     )
   }
