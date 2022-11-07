bbc1753d1f18ed629ff3f677cc640008614a1eb7
Syed Asrar Zahoor <81914950+Syed-Asrar@users.noreply.github.com>
2022-11-07 21:54:33 +0530

    added news api

--- src/components/News.js
+++ src/components/News.js
@@ -1,19 +1,26 @@
-import React, { Component} from 'react'
+import React, {Component} from 'react'
 import Newscard from './Newscard'
 
 export default class News extends Component {
-    // constructor(){
-    //     super();
-    //     this.state = {
-    //         articles: this.props.articles,
-    //     }
-    // }
+    constructor(props) {
+        super(props);
+        this.state = { 
+            articles: this.props.articles,
+            loading: false 
+        };
+      }
+    async componentDidMount(){
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=e03e9a9714a04551bcfd8c30d11db515";
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({articles: parsedData.articles})
+    }
     render() {
         return (
             <div className="container my-3">
                 <h2>NewsDog - Top headlines</h2>
                 <div className='row my-3'>
-                    {this.props.articles.map(e => {
+                    {this.state.articles.map(e => {
                       return  <div className='col-md-4' key={e.url}>
                         <Newscard title={e.title} imgUrl={e.urlToImage} description={e.description} newsUrl={e.url}/>
                     </div>})}  
